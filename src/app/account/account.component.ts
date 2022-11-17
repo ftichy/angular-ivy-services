098b96f56cc4b03e1f8bfb678417ef993aea39d9
ftichy <59052141+ftichy@users.noreply.github.com>
2022-11-17 13:46:57 +0100

    Initial commit

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input } from '@angular/core';
+import { AccountService } from '../account.service';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+})
+export class AccountComponent {
+  @Input() account: { name: string; status: string };
+  @Input() id: number;
+
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountService
+  ) {}
+
+  onSetTo(status: string) {
+    this.accountsService.updateStatus(this.id, status);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountService } from './account.service';
+import { LoggingService } from './logging.service';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, AccountComponent, NewAccountComponent],
+  providers: [AccountService, LoggingService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
